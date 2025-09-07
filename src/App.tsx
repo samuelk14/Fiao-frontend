10029104d713b5962eb7459e23763a98db9a6290
samuelk14 <smho-rey14@hotmail.com>
2025-09-07 17:09:44 -0500

    add splash screen UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import { Container, Typography, Button } from "@mui/material";
+
+import SplashScreen from "./features/splash/SplashScreen";
 import './App.css'
 
 function App() {
-  return (
-    <Container maxWidth="sm" sx={{ textAlign: "center", mt: 8 }}>
-      <Typography variant="h4" gutterBottom>
-        Bienvenido a Fiao 📲
-      </Typography>
-      <Button variant="contained" color="primary">
-        Iniciar
-      </Button>
-    </Container>
-  );
+  return <SplashScreen />;
 }
 
 export default App;
